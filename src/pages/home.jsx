f8b73d680e013c894edf3168ca1fd03d9f38d255
darkthecross <darkthecross@gmail.com>
2024-10-21 02:39:20 -0700

    Video...

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,48 +1,36 @@
 import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
 import Navbar from "./navbar";
-import viteLogo from '/vite.svg'
-import './home.css'
-
+import Demo from "./demo";
+import './home.css';
 
 function Home() {
-    const [count, setCount] = useState(0)
+    const [processed, setProcessed] = useState(false);
 
     return (
         <>
-            <section class="text-center container-fluid">
-            <Navbar />
-                <div class="row py-lg-5">
-                    <div class="col-lg-6 col-md-8 mx-auto">
-                        <h1 class="fw-light">Album example</h1>
-                        <p class="lead text-body-secondary">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-                        <p>
-                            <a href="#" class="btn btn-primary my-2">Main call to action</a>
-                            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-                        </p>
+            <div className="container-fluid">
+                <Navbar />
+                <div className="row py-lg-4">
+                    <div className="col-12">
+                        <h1 className="fw-light px-lg-5">PongSensei</h1>
+                        <p className="lead text-body-secondary px-lg-5">Revolutionizing table tennis video analysis.</p>
                     </div>
                 </div>
-            </section>
-            {/* <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
+                <div className="row py-lg-4">
+                    <div className="col-2"/>
+                    <div className="col-8">
+                    <button className="btn btn-primary" onClick={() => setProcessed(!processed)}>Analyze the demo</button>
+                    </div>
+                    <div className="col-2"/>
+                </div>
+                <div className="row py-lg-4">
+                    <div className="col-2" />
+                    <div className="col-8">
+                        <Demo processed={processed} />
+                    </div>
+                    <div className="col-2" />
+                </div>
             </div>
-            <h1>Vite + React</h1>
-            <div>
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p>
-                Click on the Vite and React logos to learn more
-            </p> */}
         </>
     );
 };
--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -3,14 +3,14 @@ import { Outlet, Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4 px-4">
-        <a class="navbar-brand" href="#">PongSensei</a>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4 px-4">
+        <a className="navbar-brand" href="#">PongSensei</a>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
               <Link className="nav-link" to="/">Home</Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link className="nav-link" to="/about">About</Link>
             </li>
           </ul>
