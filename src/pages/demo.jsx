aa3401758f145b96b5edeaca65ff84578281941d
darkthecross <darkthecross@gmail.com>
2025-08-08 00:37:31 -0700

    Update demo!

--- src/pages/demo.jsx
+++ src/pages/demo.jsx
@@ -1,28 +1,43 @@
 import ReactPlayer from 'react-player/file';
-import demo_original from '../assets/demo_original.mp4';
-import demo_processed from '../assets/demo_processed.mp4';
-
+import left_serve from '../assets/left_serve.png';
+import right_serve from '../assets/right_serve.png';
+import left_winning from '../assets/left_winning.png';
+import right_winning from '../assets/right_winning.png';
 import Navbar from "./navbar";
 import Footer from "./footer";
 
 const Demo = ({ processed }) => {
-    // let original_style = processed ? "d-none" : "d-block";
-    // let demo_style = processed ? "d-block" : "d-none";
-    let original_style = "d-none";
-    let demo_style = "d-block";
     return (
         <>
             <Navbar />
             <div className="container-fluid main">
                 <div className="row py-lg-4">
-                    {/* <div className="col-12">
-                        <ReactPlayer className={`player player-orginal ${original_style}`} key={original_style} url={demo_original} playing={true} muted={true} loop={true} width="100%" height="auto" />
+                    <div className="col-8 offset-2">
+                        <iframe width="960" height="540" src="https://www.youtube.com/embed/bol3ocU27Vs?si=_uTPr-xFiwVR5np9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                     </div>
-                    <div className="col-12">
-                        <ReactPlayer className={`player player-demo ${demo_style}`} key={demo_style} url={demo_processed} playing={true} muted={true} loop={true} width="100%" height="auto" />
-                    </div> */}
-                    <div className="col-12">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/RU4qSbICbzs?si=oXdmRsbjgJGG7qXR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
+                <div className="row py-lg-4">
+                    <h1 className="fw-light px-lg-5 main_content">Left player serve placements</h1>
+                    <div className="col-8 offset-2">
+                        <img className="img-fluid" src={left_serve} alt="Left player serve placements" />
+                    </div>
+                </div>
+                <div className="row py-lg-4">
+                    <h1 className="fw-light px-lg-5 main_content">Right player serve placements</h1>
+                    <div className="col-8 offset-2">
+                        <img className="img-fluid" src={right_serve} alt="Right player serve placements" />
+                    </div>
+                </div>
+                <div className="row py-lg-4">
+                    <h1 className="fw-light px-lg-5 main_content">Left player winning placements</h1>
+                    <div className="col-8 offset-2">
+                        <img className="img-fluid" src={left_winning} alt="Left player winning placements" />
+                    </div>
+                </div>
+                <div className="row py-lg-4">
+                    <h1 className="fw-light px-lg-5 main_content">Right player winning placements</h1>
+                    <div className="col-8 offset-2">
+                        <img className="img-fluid" src={right_winning} alt="Right player winning placements" />
                     </div>
                 </div>
             </div>
