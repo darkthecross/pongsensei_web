9b24cfef9eadfa2434973a9a1cf601687bf0d56c
darkthecross <darkthecross@gmail.com>
2025-08-22 00:07:15 -0700

    Fix demos.

--- src/pages/demo.jsx
+++ src/pages/demo.jsx
@@ -1,18 +1,36 @@
-// import player_stats from '../assets/demo_1/player_stats.png';
-// import left_serve from '../assets/demo_1/left_serve.png';
-// import right_serve from '../assets/demo_1/right_serve.png';
-// import left_winning from '../assets/demo_1/left_winning.png';
-// import right_winning from '../assets/demo_1/right_winning.png';
+import player_stats_1 from '../assets/demo_1/player_stats.png';
+import left_serve_1 from '../assets/demo_1/left_serve.png';
+import right_serve_1 from '../assets/demo_1/right_serve.png';
+import left_winning_1 from '../assets/demo_1/left_winning.png';
+import right_winning_1 from '../assets/demo_1/right_winning.png';
+
+import player_stats_2 from '../assets/demo_2/player_stats.png';
+import left_serve_2 from '../assets/demo_2/left_serve.png';
+import right_serve_2 from '../assets/demo_2/right_serve.png';
+import left_winning_2 from '../assets/demo_2/left_winning.png';
+import right_winning_2 from '../assets/demo_2/right_winning.png';
+
 import Navbar from "./navbar";
 import Footer from "./footer";
 
 const Demo = ({ id }) => {
     var yt_link = ``;
+    var player_stats, left_serve, right_serve, left_winning, right_winning;
     console.log(id);
     if (id === 1) {
         yt_link = `https://www.youtube.com/embed/bol3ocU27Vs?si=_uTPr-xFiwVR5np9`;
+        player_stats = player_stats_1;
+        left_serve = left_serve_1;
+        right_serve = right_serve_1;
+        left_winning = left_winning_1;
+        right_winning = right_winning_1;
     } else if (id === 2) {
         yt_link = `https://www.youtube.com/embed/B_rhxbq6AUU?si=GzU_dHviYjBaiaxV`;
+        player_stats = player_stats_2;
+        left_serve = left_serve_2;
+        right_serve = right_serve_2;
+        left_winning = left_winning_2;
+        right_winning = right_winning_2;
     }
     return (
         <>
@@ -26,31 +44,31 @@ const Demo = ({ id }) => {
                 <div className="row py-lg-4">
                     <h1 className="fw-light px-lg-5 main_content">Player Stats</h1>
                     <div className="col-8 offset-2">
-                        <img className="img-fluid" src={`src/assets/demo_${id}/player_stats.png`} alt="Player Stats" />
+                        <img className="img-fluid" src={player_stats} alt="Player Stats" />
                     </div>
                 </div>
                 <div className="row py-lg-4">
                     <h1 className="fw-light px-lg-5 main_content">Left player serve placements</h1>
                     <div className="col-8 offset-2">
-                        <img className="img-fluid" src={`src/assets/demo_${id}/left_serve.png`} alt="Left player serve placements" />
+                        <img className="img-fluid" src={left_serve} alt="Left player serve placements" />
                     </div>
                 </div>
                 <div className="row py-lg-4">
                     <h1 className="fw-light px-lg-5 main_content">Right player serve placements</h1>
                     <div className="col-8 offset-2">
-                        <img className="img-fluid" src={`src/assets/demo_${id}/right_serve.png`} alt="Right player serve placements" />
+                        <img className="img-fluid" src={right_serve} alt="Right player serve placements" />
                     </div>
                 </div>
                 <div className="row py-lg-4">
                     <h1 className="fw-light px-lg-5 main_content">Left player winning placements</h1>
                     <div className="col-8 offset-2">
-                        <img className="img-fluid" src={`src/assets/demo_${id}/left_winning.png`} alt="Left player winning placements" />
+                        <img className="img-fluid" src={left_winning} alt="Left player winning placements" />
                     </div>
                 </div>
                 <div className="row py-lg-4">
                     <h1 className="fw-light px-lg-5 main_content">Right player winning placements</h1>
                     <div className="col-8 offset-2">
-                        <img className="img-fluid" src={`src/assets/demo_${id}/right_winning.png`} alt="Right player winning placements" />
+                        <img className="img-fluid" src={right_winning} alt="Right player winning placements" />
                     </div>
                 </div>
             </div>
